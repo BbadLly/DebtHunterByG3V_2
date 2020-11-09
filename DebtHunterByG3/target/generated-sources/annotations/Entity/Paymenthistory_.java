2c0fb9cb451ced748a084cacd3207ce7ce66f1b7
guidekai <73281494+guidekai@users.noreply.github.com>
2020-11-09 23:00:43 +0700

    11.00 9/11/202
    ลบ index

--- DebtHunterByG3/target/generated-sources/annotations/Entity/Paymenthistory_.java
+++ DebtHunterByG3/target/generated-sources/annotations/Entity/Paymenthistory_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-11-09T22:21:39")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-11-09T22:59:05")
 @StaticMetamodel(Paymenthistory.class)
 public class Paymenthistory_ { 
 
